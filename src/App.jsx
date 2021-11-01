5be7d82ef1a0a07304faa11dc4a98a42b8c3f9f5
JhonatanGAlves <jhonatangalves96@gmail.com>
2021-11-01 15:13:19 -0300

    styling cards through props color

--- src/App.jsx
+++ src/App.jsx
@@ -19,18 +19,18 @@ export const App = () => {
       <h1>Fundamentos React</h1>
 
       <div className="Cards">
-        <Card title="#04 - Desafio número aleatório">
+        <Card title="#04 - Desafio número aleatório" color="#DC1360" /*Estou passando uma cor por props*/ >
           <RandomNumber //Passei este compenente dentro do componente Card, mas para que ele fique acessível, preciso ir até o arquivo do componente Card e passar uma props.children
             min={ 0 }
             max={ 10 }
           />
         </Card>
 
-        <Card title="#03 - Fragmento">
+        <Card title="#03 - Fragmento" color="#00A89F" /*Estou passando uma cor por props*/ >
           <UsingFragment />
         </Card>
 
-        <Card title="#02 - Comp. com Parâmetro">
+        <Card title="#02 - Comp. com Parâmetro" color="#00B4E1" /*Estou passando uma cor por props*/ >
           <CompWithParameter
             title={"Situação do Aluno"}
             name={ name }
@@ -39,7 +39,7 @@ export const App = () => {
           />
         </Card>
 
-        <Card title="#01 - Primeiro Componente">
+        <Card title="#01 - Primeiro Componente" color="#8B479E" /*Estou passando uma cor por props*/ >
           <FirstComponent />
         </Card>
       </div>
--- src/Components/layout/Card.jsx
+++ src/Components/layout/Card.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import './Card.css'
 
 export const Card = (props) => {
+
+    /*Ao declarar a props "color" no component Card onde está sendo chamado, preciso passar a propriedade style dentro da DIV
+    e passar meu objeto que recebeu os estilos seguido da props.color*/
+    const cardStyle = {
+        backgroundColor: props.color || '#353535',
+        borderColor: props.color || '#353535'
+    }
+
     return (
-        <div className="Card">
+        <div className="Card" style={ cardStyle }>
             <div className="Title">{props.title}</div>
             <div className="Content">
                 {props.children}
